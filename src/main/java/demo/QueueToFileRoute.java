e7ada1579c9e93f9521d94a7e37ea73a51687363
Mark Sanders <masander@capgemini.com>
2015-04-23 14:06:44 +0100

    Configure destination directory with application.properties

--- src/main/java/demo/QueueToFileRoute.java
+++ src/main/java/demo/QueueToFileRoute.java
@@ -5,8 +5,9 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class QueueToFileRoute extends RouteBuilder {
+	
 	@Override
 	public void configure() throws Exception {
-		from("test-jms:queue:test.queue").beanRef("foo", "transform").to("file://test");
+		from("test-jms:queue:test.queue").beanRef("foo", "transform").to("file://{{destination.file}}");
 	}
 }
