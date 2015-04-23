9e83364bd6d6b8c9da997e12d649604747b1312f
Mark Sanders <masander@capgemini.com>
2015-04-23 14:15:06 +0100

    Remove @Configuration from main class

--- src/main/java/demo/SpringBootCamelConfigureApplication.java
+++ src/main/java/demo/SpringBootCamelConfigureApplication.java
@@ -2,10 +2,8 @@ package demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
-@Configuration
 public class SpringBootCamelConfigureApplication {
 	
 	public static void main(String[] args) {
